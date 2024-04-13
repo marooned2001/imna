07f58b1a5c60023c79e6831e0c1886587dc7af30
kaveh <kavehhn174@gmail.com>
2024-04-13 12:11:37 +0330

    ADD: Frontend structure files

--- imnapr/newspaper/views.py
+++ imnapr/newspaper/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def news(request):
+    return render(request, 'home.html')
+
+def contact_us(request):
+    return render(request, 'home.html')
+
+def about_us(request):
+    return render(request, 'home.html')
+
